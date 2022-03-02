9dd0667389c278e2798abaeed5d44f2e11ef1344
shindhha <guillaume.medard@iut-rodez.fr>
2022-03-02 23:40:34 +0100

    Grosse avancé sur la classe Sac.java

---
+++ Jouet.java
@@ -0,0 +1,25 @@
+package orienteo;
+public class Jouet {
+
+	private int masse;
+	private String nom;
+	private int plagemin;
+	private int plagesup;
+
+
+	public Jouet(int masse, String nom, int plagesup , int plagemin) {
+
+		this.masse = masse;
+		this.nom = nom;
+		this.plagemin = plagemin;
+		this.plagesup = plagesup;
+	}
+
+	public String getName() {
+		return this.nom;
+	}
+
+	public int getMasse() {
+		return this.masse;
+	}
+}
